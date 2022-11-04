385411effd811a23b46697d853f0f92c9ca073ef
Oluwasegun Ige <mailshegze@gmail.com>
2022-11-04 10:43:29 +0100

    feat: Added post method for operations

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,6 +8,28 @@ def get_user_info():
                     'backend':True,
                     'age':21,
                     'bio':'I am a backend developer participating in the HNG9 internship.'})
+
+@app.route('/operate', methods=['POST'])
+def operate():
+    if not request.json or not 'operation_type' in request.json or not 'x' in request.json or not 'y' in request.json:
+        abort(400)
+    operation = {
+        'operation_type': request.json['operation_type'],
+        'x': request.json['x'],
+        'y': request.json['y']
+    }
+    if operation['operation_type'] == 'addition':
+        result = operation['x'] + operation['y']
+    elif operation['operation_type'] == 'subtraction':
+        result = operation['x'] - operation['y']
+    elif operation['operation_type'] == 'multiplication':
+        result = operation['x'] * operation['y']
+    else:
+        abort(400)
+    return jsonify({'slackUsername': 'tshergzeh',
+        'operation_type': operation['operation_type'],
+        'result': result
+    })
 
 @app.route('/')
 def hello_world():
